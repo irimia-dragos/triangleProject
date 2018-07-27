74e0184eda8126dc3ecdb0cc2a73674471be53ce
Dragos Irimia <dragos.irimia@uxpsystems.com>
2018-07-27 23:03:30 +0300

    fix jmh test suite

--- jmh/src/main/java/com/github/irimiadragos/tests/jmh/TriangleBenchmark.java
+++ jmh/src/main/java/com/github/irimiadragos/tests/jmh/TriangleBenchmark.java
@@ -33,6 +33,7 @@ package com.github.irimiadragos.tests.jmh;
 
 import com.github.irimiadragos.tests.Triangle;
 import org.openjdk.jmh.annotations.Benchmark;
+import org.openjdk.jmh.infra.Blackhole;
 import org.openjdk.jmh.runner.Runner;
 import org.openjdk.jmh.runner.RunnerException;
 import org.openjdk.jmh.runner.options.Options;
@@ -41,8 +42,23 @@ import org.openjdk.jmh.runner.options.OptionsBuilder;
 public class TriangleBenchmark {
 
     @Benchmark
-    public void testMethod() {
-        Triangle t = new Triangle(1, 2, 3);
+    public void testScaleneMethod(Blackhole bh) {
+        Triangle t = new Triangle(3, 4, 5);
+        bh.consume(t.getType());
+    }
+
+
+    @Benchmark
+    public void testEquilaterale(Blackhole bh) {
+        Triangle t = new Triangle(5, 5, 5);
+        bh.consume(t.getType());
+    }
+
+
+    @Benchmark
+    public void testIsosceleMethod(Blackhole bh) {
+        Triangle t = new Triangle(3, 5, 5);
+        bh.consume(t.getType());
     }
 
     public static void main(String[] args) throws RunnerException {
